67b11d3db438cdcbd5e59e46c74d6550b5d39f30
Crabe05 <guillaume.dupont@etu.enseeiht.fr>
2017-03-01 16:45:39 +0100

    (Dummy)

--- edu/mit/csail/sdg/alloy4graph/GraphViewer.java
+++ edu/mit/csail/sdg/alloy4graph/GraphViewer.java
@@ -283,10 +283,8 @@ public final strictfp class GraphViewer extends JPanel {
                     if (highlight != null && highlight instanceof AbstractGraphElement)
                         ((AbstractGraphElement)highlight).setHighlight(false); //[N7-G.Dupont]
                     highlight = obj;
-                    if (obj instanceof AbstractGraphElement) {
+                    if (obj instanceof AbstractGraphElement)
                         ((AbstractGraphElement)highlight).setHighlight(true); //[N7-G.Dupont]
-                        System.out.println("Entering " + ((AbstractGraphElement)highlight).uuid);
-                    }
                     
                     needRepaint = true;
                 }
