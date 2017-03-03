a9939177f12efc3007affafdda853db9f742fe06
Bossut Remi <remi.bossut@etu.enseeiht.fr>
2017-03-03 09:56:13 +0100

    Highlighting subnodes is now possible.

--- edu/mit/csail/sdg/alloy4graph/GraphNode.java
+++ edu/mit/csail/sdg/alloy4graph/GraphNode.java
@@ -813,11 +813,19 @@ public strictfp class GraphNode extends AbstractGraphNode {
             gr.setColor(color);
         }
         
+        Object high = null;
+        for (GraphNode n : getChildren()){
+          if (n.highlight()){
+            high = n;
+            break;
+          }
+        }
+       
         gr.draw(poly, true);
         gr.setColor(Color.BLACK);
         gr.draw(poly, false);
         gr.translate(subLeft, subTop);
-        subGraph.draw(gr, scale, uuid, true);
+        subGraph.draw(gr, scale, high, true);
         gr.translate(-subLeft, -subTop);
 
         /*
